fa52b984a5ebd1b8424a2a1dbb30eef7fd5180df
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2023-09-07 21:10:10 -0500

    Update to have undirected test uncommented and remove line wraps from provided code.

--- test_genGraphAsDict.py
+++ test_genGraphAsDict.py
@@ -2,16 +2,19 @@ from unittest import TestCase
 from genGraphAsDict import build_graph_as_dict
 
 # TESTs for creating Graph Data Structure (Python dict)
-# Recommend working on directed graph first, then uncomment 2nd test fn for undirected graph
+# Recommend working on directed graph first, then uncomment 2nd test fn
+# for undirected graph
 
 class Test(TestCase):
 
     def test_build_directed_graph_as_dict(self):
-        node_list = [('A', 'B'), ('B', 'C'), ('C', 'D'), ('C', 'E'), ('A', 'E'), ('B', 'F')]
+        node_list = [('A', 'B'), ('B', 'C'), ('C', 'D'), ('C', 'E'),
+                     ('A', 'E'), ('B', 'F')]
 
         graph_dict = build_graph_as_dict(node_list, directed_graph=True)
 
-        # test that a key exists for each of the first nodes in node_list tuples (from->to)
+        # test that a key exists for each of the first nodes in
+        # node_list tuples (from->to)
         assert ('A' in graph_dict.keys()) == True
         assert ('B' in graph_dict.keys()) == True
         assert ('C' in graph_dict.keys()) == True
@@ -26,15 +29,15 @@ class Test(TestCase):
 
         print ("Passed all tests")
 
-
-''' Uncomment this test when you have implemented for undirected graphs
-
     def test_build_UNdirected_graph_as_dict(self):
-        node_list = [('A', 'B'), ('B', 'C'), ('C', 'D'), ('C', 'E'), ('A', 'E'), ('B', 'F')]
+        node_list = [('A', 'B'), ('B', 'C'), ('C', 'D'), ('C', 'E'),
+                     ('A', 'E'), ('B', 'F')]
 
-        graph_dict = build_graph_as_dict(node_list, isDirectedGraph=False)
+        graph_dict = build_graph_as_dict(node_list,
+                                         directed_graph=False)
 
-        # test that a key exists for each of the first nodes in node_list tuples (from->to)
+        # test that a key exists for each of the first nodes in
+        # node_list tuples (from->to)
         assert ('A' in graph_dict.keys()) == True
         assert ('B' in graph_dict.keys()) == True
         assert ('C' in graph_dict.keys()) == True
@@ -54,6 +57,5 @@ class Test(TestCase):
         # test that we have correct children for 'F'
         child_list = graph_dict['F']
         assert ('B' in child_list) == True
-        
+
         print ("Passed all tests")
-'''
