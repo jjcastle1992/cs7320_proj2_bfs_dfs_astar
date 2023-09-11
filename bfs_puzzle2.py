fc5d040180869ef7a0712f1b10f3864b50ba38e9
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2023-09-11 15:14:27 -0500

    Updated print statements. Need to update logic to create boards before going into BFS

--- bfs_puzzle2.py
+++ bfs_puzzle2.py
@@ -72,12 +72,12 @@ def get_child_boards_list(board):
 
     # KNOWN POTENTIAL BUGS: NOT BOUNDING FOR EDGE CASES
 
-    # determine if we can move up (zero position (i, j) where i < R-1
+    # determine if we can move up (Zero not in bottom Row)
     # print(f'This Level Start Board: {board}')
     up_board = copy.deepcopy(board)
     # print('\nBEGIN POSSIBLE MOVES: ')
 
-    if (zero_position[0] < (total_board_rows - 1)):
+    if (zero_position[0] != (total_board_rows - 1)):
         swap_piece = up_board[zero_position[0] + 1][zero_position[1]]
         # print(f'Swap Up: {swap_piece}')
         # swap zero
@@ -94,10 +94,10 @@ def get_child_boards_list(board):
         else:
             list_of_child_boards[str(board)] = [up_board]
 
-    # determine if we can move down (zero position (i, j) where i > 0
+    # determine if we can move down (Zero not in top row)
     down_board = copy.deepcopy(board)
 
-    if (zero_position[0] > 0):
+    if (zero_position[0] != 0):
         swap_piece = down_board[zero_position[0] - 1][zero_position[1]]
         # print(f'Swap Down: {swap_piece}')
         # swap zero
@@ -114,10 +114,10 @@ def get_child_boards_list(board):
         else:
             list_of_child_boards[str(board)] = [down_board]
 
-    # determine if we can move left (zero position (i, j) where j < C-1
+    # determine if we can move left (Zero cannot be in last column)
     left_board = copy.deepcopy(board)
 
-    if (zero_position[1] < (total_board_cols - 1)):
+    if (zero_position[1] != (total_board_cols - 1)):
         swap_piece = left_board[zero_position[0]][zero_position[1] + 1]
         # print(f'Swap Left: {swap_piece}')
         # swap zero
@@ -134,10 +134,10 @@ def get_child_boards_list(board):
         else:
             list_of_child_boards[str(board)] = [left_board]
 
-    # determine if we can move right (zero position (i, j) where j > 0
+    # determine if we can move right (Zero cannot be in first column)
     right_board = copy.deepcopy(board)
 
-    if (zero_position[1] > 0):
+    if (zero_position[1] != 0):
         swap_piece = right_board[zero_position[0]][zero_position[1] - 1]
         # print(f'Swap Right: {swap_piece}')
         # swap zero
@@ -161,19 +161,19 @@ def bfs_shortest_paths(graph, start, goal):
     global bfs_nodes_visited  # to track number nodes visited
 
     queue = [[start]]
-    bfs_nodes_visited = 0
 
     while queue:
         path = queue.pop(0)
         vertex = path[-1]
         bfs_nodes_visited += 1
-        next_board_list = [x for x in graph[str(vertex)]
-                           if str(x) not in set(str(path))]
-        for next in next_board_list:
-            if next == goal:
-                return [path + [next]]
-            else:
-                queue.append(path + [next])
+        if str(vertex) in graph:
+            next_board_list = [x for x in graph[str(vertex)]
+                               if str(x) not in set(str(path))]
+            for next in next_board_list:
+                if next == goal:
+                    return [path + [next]]
+                else:
+                    queue.append(path + [next])
 
     return None  # No shortest path was found
 
@@ -188,6 +188,8 @@ def bfs2(start_board, goal_board):
     explored_boards = []  # note: should explore other ds for find speed
     open_boards = []
     shortest_path = None
+    global bfs_nodes_visited  # to track number nodes visited
+    bfs_nodes_visited = 0
     # Sequence:
     #     1. Set current_board = start_board
     #     2. (in final ver). *** WAIT for NOW***
@@ -219,10 +221,10 @@ def bfs2(start_board, goal_board):
                 open_boards.append(child)
                 num_children += 1
 
-        print('**Current Board**')
+        print(f'**Current Board{bfs_level - 1}**')
         matrix_printer([current_board])
-        print(f'Total Child Boards at Level {bfs_level}: {num_children}')
-        print('**Child Boards to explore**')
+        print(f'Total Unique Child Boards at Level {bfs_level}: {num_children}')
+        print('**All Current Child Boards to Explore**')
         matrix_printer(open_boards, 1)
 
         open_boards_dict = {str(open_boards[0]): open_boards[1:]}
@@ -238,6 +240,9 @@ def bfs2(start_board, goal_board):
     #         1. shortest_path found
     #         2. open_boards is empty (no legal child boards left)
 
+    print(f'Levels explored: {bfs_level}')
+    print(f'Explored Node List Len: {len(explored_boards)}')
+    print(f'Open Board List len{len(open_boards)}')
 
     return shortest_path
 
@@ -261,11 +266,12 @@ def matrix_printer(matrix, start_index=0, shortest_path = False):
         index += 1
 
 def main():
-    start_state = [[4, 1, 3], [2, 0, 6], [7, 5, 8]]
-    # start_state = [[1, 2, 3], [4, 5, 6], [7, 0, 8]]
+    global bfs_nodes_visited
+    # start_state = [[4, 1, 3], [2, 0, 6], [7, 5, 8]]
+    start_state = [[1, 2, 3], [4, 0, 6], [7, 5, 8]]
     goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]  # my assumed goal
     shortest_path = bfs2(start_state, goal_state)
-
+    print(f'Total Nodes Explored: {bfs_nodes_visited}')
     if (shortest_path != None):
         print(f'Shortest Path')
         matrix_printer(shortest_path, 0, True)
